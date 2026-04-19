cdb77d7ee441f06f9dd12371849de07d9f445921
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-20 02:31:14 +0700

    Bai toan thap Ha Noi

--- Đệ quy và khử đệ quy/Giải bài toán tháp Hà Nội/TowerOfHanoiSolver.java
+++ Đệ quy và khử đệ quy/Giải bài toán tháp Hà Nội/TowerOfHanoiSolver.java
@@ -1,17 +1,27 @@
 public class TowerOfHanoiSolver {
 
     /**
-     * In ra các bước giải bài toán Tháp Hà Nội với n đĩa.
-     *
-     * Mỗi bước được in ra trên một dòng theo cú pháp:
-     * Move disk from X to Y
-     *
-     * @param n    số lượng đĩa cần di chuyển
-     * @param from cọc nguồn
-     * @param to   cọc đích
-     * @param aux  cọc trung gian
+     * Thêm từ khóa 'static' để có thể gọi trực tiếp từ hàm main
      */
-    public void solve(int n, char from, char to, char aux) {
-        // TODO
+    public static void solve(int n, char from, char to, char aux) {
+        if (n == 1) {
+            // Thêm khoảng trắng quanh chữ "sang"
+            System.out.println("Move disk 1 from " + from + " to " + to);
+        } else {
+            // Bước 1: Di chuyển n-1 đĩa từ nguồn sang trung gian
+            solve(n - 1, from, aux, to);
+            
+            // Bước 2: Di chuyển đĩa thứ n từ nguồn sang đích
+            System.out.println("Move disk " + n + " from " + from + " to " + to);
+            
+            // Bước 3: Di chuyển n-1 đĩa từ trung gian sang đích
+            solve(n - 1, aux, to, from);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nDisks = 3; 
+        // Gọi hàm tĩnh (static) một cách hợp lệ
+        solve(nDisks, 'A', 'C', 'B'); 
     }
 }
