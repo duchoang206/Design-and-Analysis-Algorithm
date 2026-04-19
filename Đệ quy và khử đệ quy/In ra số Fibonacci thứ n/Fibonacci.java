f1965c68f46e4d2778cf1779643fa6bc48611e23
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-20 04:23:37 +0700

    Upload toan bo code DAA HUS 2026

--- Đệ quy và khử đệ quy/In ra số Fibonacci thứ n/Fibonacci.java
+++ Đệ quy và khử đệ quy/In ra số Fibonacci thứ n/Fibonacci.java
@@ -7,8 +7,12 @@ public class Fibonacci {
      * @return giá trị Fibonacci thứ n
      */
     public int fibonacciRecursive(int n) {
-        // TODO
-        return 0;
+        if ( n ==0) return 0;
+        if ( n ==1) return 1;
+        else {
+            return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+        }
+        
     }
 
     /**
@@ -18,7 +22,16 @@ public class Fibonacci {
      * @return giá trị Fibonacci thứ n
      */
     public int fibonacciIterative(int n) {
-        // TODO
-        return 0;
+        if ( n ==0 )return 0;
+        if ( n ==1 ) return 1;
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 1;
+        for ( int i = 2; i < n ; i++) {
+            fn = f0 + f1;
+            f0 =f1;
+            f1 =fn;
+        }
+        return fn;
     }
 }
