c0ba02cfbc9d5faf98aa3339651c295a6ed5608e
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-20 09:40:04 +0700

    bai toan Chuot trong me cung

--- Thử sai/Chuột trong mê cung (DFS trá hình)/RatInAMaze.java
+++ Thử sai/Chuột trong mê cung (DFS trá hình)/RatInAMaze.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class RatInAMaze {
+    private static final int[] dRow = {1, 0, 0, -1};
+    private static final int[] dCol = {0, -1, 1, 0};
+    private static final String[] dir = {"D", "L", "R", "U"};
 
     /**
      * Cho ma trận nhị phân vuông mat[][] biểu diễn mê cung. 
@@ -13,6 +18,36 @@ public class RatInAMaze {
      * @param maze  ma trận 0/1 
      */
     public void findPaths(int n, int[][] maze) {
-        // TODO
+        ArrayList<String> kq = new ArrayList<>();
+        boolean[][] visited = new boolean[n][n];
+        if ( maze[0][0] ==1 ) {
+            solve( 0,0,n,maze,visited,"", kq);
+        }
+        if ( kq.isEmpty()) {
+            System.out.println("-1");
+        } else {
+            for ( String path : kq ) {
+                System.out.println(path);
+            }
+        }
+    }
+    public void solve(int r , int c , int n , int[][] maze , boolean[][] visited , String path , ArrayList<String> kq ) {
+        if ( r == n-1 && c == n-1) {
+            kq.add(path);
+        }
+        visited[r][c] = true;
+        for ( int i = 0; i<4 ; i++ ) {
+            int nextR = r + dRow[i];
+            int nextC = c + dCol[i];
+        if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < n 
+                && maze[nextR][nextC] == 1 && !visited[nextR][nextC]) {
+                
+                solve(nextR, nextC, n, maze, visited, path + dir[i], kq);
+            }
+        }
+    
+    visited[r][c] = false;
+
+                // TODO
     }
 }
